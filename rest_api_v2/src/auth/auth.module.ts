607efbe92fa92822706a5685efe006880573bd71
Christopher <christopher.p@asiapro.io>
2023-12-22 16:59:19 +0800

    SCRUM-61: fix env issue

--- rest_api_v2/src/auth/auth.module.ts
+++ rest_api_v2/src/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
@@ -9,6 +10,7 @@ import { JwtStrategy } from './jwt.strategy';
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
+    ConfigModule.forRoot(),
     JwtModule.register({
       secret: process.env.AUTH_SECRET_KEY,
       signOptions: {
