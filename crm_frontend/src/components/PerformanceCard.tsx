9ad8e92013848c3e7081cf1e4416e6cfed3d9853
Christopher <christopher.p@asiapro.io>
2024-01-05 11:29:07 +0800

    SCRUM-85: additional fixes

--- crm_frontend/src/components/PerformanceCard.tsx
+++ crm_frontend/src/components/PerformanceCard.tsx
@@ -1,11 +1,12 @@
 import {
   Card,
   CardBody,
+  Skeleton,
   Text,
   Box
 } from '@chakra-ui/react';
 
-export const PerformanceCard = ({ label, movementPercent, value }) => {
+export const PerformanceCard = ({ label, movementPercent, value, isLoading }) => {
   return (
     <Card 
       w="100%" 
@@ -16,20 +17,22 @@ export const PerformanceCard = ({ label, movementPercent, value }) => {
       rounded="lg"
     >
       <CardBody>
-        <Box>    
-          <Text color={"#374A16"} as={"b"} fontSize={"lg"} align={"center"}>
-            {label}
-          </Text>
-        </Box>    
-        <Box align="center" mt={2}>
-          <Text color={"#374A16"} as={"b"} fontSize={"xl"} align={"center"}>{value}</Text>
-        </Box>    
-        <Box align="center">
-          <Text color={"#374A16"} fontSize={"xs"} align={"center"}>
-            { movementPercent >= 0 ? '+' : '' }
-            {movementPercent.toFixed(2)} %
-          </Text>
-        </Box>    
+        <Skeleton isLoaded={!isLoading}>
+          <Box>    
+            <Text color={"#374A16"} as={"b"} fontSize={"lg"} align={"center"}>
+              {label}
+            </Text>
+          </Box>    
+          <Box align="center" mt={2}>
+            <Text color={"#374A16"} as={"b"} fontSize={"xl"} align={"center"}>{value}</Text>
+          </Box>    
+          <Box align="center">
+            <Text color={"#374A16"} fontSize={"xs"} align={"center"}>
+              { movementPercent >= 0 ? '+' : '' }
+              {movementPercent.toFixed(2)} %
+            </Text>
+          </Box>    
+        </Skeleton>
       </CardBody>
     </Card>
   );
